50ec004d36e9c596e9df6ef8032fb330d0e72031
WeiMin-Coding <badmanweimin@gmail.com>
2024-08-09 00:49:14 +0800

    结构体方法练习

--- HanShunping-GolangCore/chapter10/7-MethodExercise02/methodExercise02.go
+++ HanShunping-GolangCore/chapter10/7-MethodExercise02/methodExercise02.go
@@ -7,7 +7,7 @@ import "fmt"
 type MethodUtils struct {
 }
 
-func (MethodUtils MethodUtils) printRectangle() {
+func (MethodUtils *MethodUtils) printRectangle() {
 	for i := 0; i < 8; i++ {
 		for j := 0; j < 10; j++ {
 			fmt.Print("*  ")
@@ -16,7 +16,7 @@ func (MethodUtils MethodUtils) printRectangle() {
 	}
 }
 
-func (MethodUtils MethodUtils) printRectangle2(m, n int) {
+func (MethodUtils *MethodUtils) printRectangle2(m, n int) {
 	for i := 0; i < m; i++ {
 		for j := 0; j < n; j++ {
 			fmt.Print("*  ")
@@ -25,8 +25,22 @@ func (MethodUtils MethodUtils) printRectangle2(m, n int) {
 	}
 }
 
+func (MethodUtils *MethodUtils) RectangleCalculation(length, width float64) float64 {
+	return length * width
+}
+
+func (MethodUtils *MethodUtils) IsEven(number int) bool {
+	if number%2 == 0 {
+		return true
+	}
+	return false
+}
+
 func main() {
 	var methodUtils MethodUtils
-	//methodUtils.printRectangle()
+	methodUtils.printRectangle()
 	methodUtils.printRectangle2(5, 6)
+	res := methodUtils.RectangleCalculation(5.4, 6)
+	fmt.Println("Result:", res)
+	fmt.Println("IsOddAndEven:", methodUtils.IsEven(2))
 }
